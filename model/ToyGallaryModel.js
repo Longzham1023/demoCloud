302bd6b628c865a15750132190ccf1bf88b91e6f
Longzham1023 <trannamlong203@gmail.com>
2023-06-27 11:35:00 +0700

    update

--- model/ToyGallaryModel.js
+++ model/ToyGallaryModel.js
@@ -4,6 +4,9 @@ var ToyGallerySchema = new mongoose.Schema({
   name: String,
   price: Number,
   img: String,
+  quantity: Number,
+  cate: String,
+  vid: String
 })
 
 var ToyGalleryModel = mongoose.model('ToyGallery', ToyGallerySchema, 'ToyGallery')
